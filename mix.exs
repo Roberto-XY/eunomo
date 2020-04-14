5288f48ca99eb0dd934514207a290264a79d8512
roberto <a.robertofischer@gmail.com>
2020-04-14 12:38:23 +0200

    FIX elixir version 1.10

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Eunomo.MixProject do
   def project do
     [
       app: :eunomo,
-      version: "0.1.0",
-      elixir: ">= 1.8.0",
+      version: "0.1.1",
+      elixir: ">= 1.10.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: [
