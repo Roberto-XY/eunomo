245b867d46c2499112b056916213b02fa6b3220e
roberto <a.robertofischer@gmail.com>
2022-09-20 23:35:14 +0200

    MOD update elixir version req

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Eunomo.MixProject do
   def project do
     [
       app: :eunomo,
-      version: "0.1.3",
-      elixir: ">= 1.10.0",
+      version: "1.0.0",
+      elixir: ">= 1.13.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: [
