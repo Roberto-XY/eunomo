43861225e97eb9e02d7926cc6fedd770743f21a0
roberto <a.robertofischer@gmail.com>
2022-09-23 00:04:51 +0200

    REM all pre 1.13.0 constructs

--- .eunomo.exs
+++
@@ -1,9 +0,0 @@
-# Used by "mix eunomo" with Elixir `< 1.13.0`
-[
-  inputs: :read_from_dot_formatter,
-  formatter: [
-    Eunomo.AliasSorter,
-    Eunomo.ImportSorter,
-    Eunomo.RequireSorter
-  ]
-]
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Eunomo.MixProject do
     [
       app: :eunomo,
       version: "1.0.0",
-      # elixir: ">= 1.13.0",
+      elixir: ">= 1.13.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: [
